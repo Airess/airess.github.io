661560f92d7433ab121d4bb1cdde949e0d19094f
Airess <beautebargains@gmail.com>
2016-06-29 12:51:33 -0400

    updated subscribe form

--- subscribe.php
+++ subscribe.php
@@ -13,8 +13,8 @@ if ( isset( $_POST['newsletter_submit'] ) ) {
 	
 	// Send email if no input errors
 	if ( empty( $newsletter_errors ) ) {
-		$email_to = "example@example.com"; // Change to your own email address
-		$subject = "Newsletter Subscription";
+		$email_to = "hello@byairess.com"; // Change to your own email address
+		$subject = "Launch Subscription";
 		$body = "Subscriber details: " . $newsletter_email . "\r\n";
 		$headers = "Newsletter Subscription <" . $email_to . ">\r\nReply-To: " . $newsletter_email . "\r\n";
 		mail( $email_to, $subject, $body, $headers );
